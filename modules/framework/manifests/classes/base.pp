87f69bb3f714c62355d886a5b2ed62dbb9dd27ee
Karl Pietri <karl@wrenchies.net>
2011-12-08 07:13:46 +0000

    rename the includes

--- modules/framework/manifests/classes/base.pp
+++ modules/framework/manifests/classes/base.pp
@@ -16,16 +16,16 @@ class framework::base {
 			ensure=>latest
 	}
 
-	include shared::log
-	include shared::epel
-	include shared::puppet
-	include shared::global_users
-	include shared::sudo
-	include shared::sshd
+	include framework::log
+	include framework::epel
+	include framework::puppet
+	include framework::global_users
+	include framework::sudo
+	include framework::sshd
 
 
 	#raw includes of mysql based resources not ready yet	but we do this any way and in the tcpwrapers class we only fetch for hosts.allow entries
-	include shared::tcpwrapers
+	include framework::tcpwrapers
 	
 	tcpwrapers::allow{"sshd: 10.0.0.0/8 127.0.0.1 192.168.0.0/16 172.16.0.0/12": }
 
