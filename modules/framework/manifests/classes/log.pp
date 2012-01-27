522f132c5b47137a38a508827135b774e713fbb8
Karl Pietri <karl@wrenchies.net>
2012-01-27 21:09:46 +0000

    debug new config class

--- modules/framework/manifests/classes/log.pp
+++ modules/framework/manifests/classes/log.pp
@@ -1,4 +1,5 @@
 class framework::log {
+	err("node($fqdn) has attributes ${config::attributes}")
 	$role = $config::attributes[role]
 	err("node($fqdn) is applying role($role) from env($environment) with file prefix($prefix)")
 }
