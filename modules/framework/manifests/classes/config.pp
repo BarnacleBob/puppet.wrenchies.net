2194410336bb7ae2f7b2992c1ef8c42b88311d29
Karl Pietri <karl@wrenchies.net>
2012-01-27 21:17:36 +0000

    run config class first

--- modules/framework/manifests/classes/config.pp
+++ modules/framework/manifests/classes/config.pp
@@ -1,6 +1,9 @@
 #class framework::config {
+stage{"first": before => Stage["main"] }
 class config($attributes) {
 	if ! $attributes[role] {
 		fail("config must contain a role")
 	}
 }
+
+Class["config"]{ stage => "first" }
