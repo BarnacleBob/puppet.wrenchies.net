68618ba0982198d1f1cd2ee59a838299c2171da5
Karl Pietri <karl@wrenchies.net>
2012-01-28 01:08:18 +0000

    attempt for config to go first wasn't a problem and doesn't work

--- modules/framework/manifests/classes/config.pp
+++ modules/framework/manifests/classes/config.pp
@@ -1,10 +1,7 @@
 #class framework::config {
-stage{"first": before => Stage["main"] }
 class config($attributes) {
 	err("config class is being evaled")
 	if ! $attributes[role] {
 		fail("config must contain a role")
 	}
 }
-
-Class["config"]{ stage => "first" }
