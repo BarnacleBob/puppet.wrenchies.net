fdf05b2dead6d07139a931295613062faa91afa0
Karl Pietri <karl@wrenchies.net>
2012-01-28 00:37:23 +0000

    prefix fix for tfile

--- modules/framework/manifests/defines/tfile.pp
+++ modules/framework/manifests/defines/tfile.pp
@@ -2,7 +2,9 @@ define tFile ($ensure="present",$backup=client,$owner=root,$group=root,$mode=644
 	$role = $config::attributes[role]
 	
 	err("manifest path is ${settings::manifest}")
-	$prefix=inline_template('<%= scope.lookupvar("settings::manifest").sub(/\/puppet.wrenchies.net\/.*$/, "") %>')
+	$envPrefix=inline_template('<%= scope.lookupvar("settings::manifest").sub(/\/site.pp$/, "") %>')
+	err("tfile envPrefix is ${envPrefix}")
+	$prefix=inline_template('<%= envPrefix.sub(/\/(puppet.wrenchies.net\/)?environments/, "") %>')
 	err("tfile prefix is ${prefix}")
 	if $ensure=="present" {
 		file{
@@ -16,9 +18,9 @@ define tFile ($ensure="present",$backup=client,$owner=root,$group=root,$mode=644
 				content=>
 					inline_template(
 						file(
-							"$prefix/environments/asdf/modules/main/files/host/${::domain}/${::hostname}/$name",
-							"$prefix/environments/realEnvironment/modules/main/files/role/$role/$name",
-							"$prefix/environments/realEnvironment/modules/main/files/all/$name",
+							"$envPrefix/modules/main/files/host/${::domain}/${::hostname}/$name",
+							"$envPrefix/modules/main/files/role/$role/$name",
+							"$envPrefix/modules/main/files/all/$name",
 							"$prefix/modules/shared/files/role/$role/$name",
 							"$prefix/modules/shared/files/all/$name",
 							"$prefix/puppet.wrenchies.net/modules/framework/files/role/$role/$name",
